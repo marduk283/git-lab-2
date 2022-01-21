f2d0f2b3564d198b73b2dca4b755477a0401bccd
Jackson Kauflin <jk491120@ohio.edu>
2022-01-21 14:42:45 -0500

    Added git-lab-program.cc poggers

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Jackson Kauflin
+ *        Date: January 21, 2022
+ * Description: Program used in the git lab.
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hey, yo! I am bucho! We are okyaku-san! Ensou shimasen!" << endl;
+    return 0;
+}// main
